f396ef2722393839fff6fdb450b178e84c14a280
Norookezi <norookezi@pandacrp.com>
2025-01-30 22:17:59 +0100

    feat: :sparkles: Added command Handler
    Created a function to parse messages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
-import { Bot } from "@twurple/easy-bot";
+import { Bot, MessageEvent } from "@twurple/easy-bot";
 import { AccessToken, RefreshingAuthProvider } from "@twurple/auth";
+
 
 // Load environment variables from .env file
 config();
@@ -32,4 +33,15 @@ const bot = new Bot({
 
 bot.onConnect(() => {
   console.log("Connected to Twitch !");
+});
+
+bot.onMessage(async (msg: MessageEvent) => {
+  if (!msg.text.startsWith("!")) return;
+
+  const [command, ...args] = msg.text.substring(1).split(" ");
+
+  switch (command) {
+    default:
+      break;
+  }
 });
