6aa0964a7320bace2087b20ac3bccbdffbb38d12
Norookezi <norookezi@pandacrp.com>
2025-01-30 21:14:45 +0100

    feat: :sparkles: Created TTS core

---
+++ src/cores/TTS.core.ts
@@ -0,0 +1,15 @@
+import { Core } from '../../core';
+
+export const TTSCore: Core = {
+    name: 'TTS',
+    storie: [],
+    creationDate: new Date(2025-1-29),
+    voice: {
+        voice: 'fr-FR-HenriNeural',
+        volume: 1,
+        pitch: 0,
+        rate: 0,
+        wordWrap: true,
+        emotion: 'happiness'
+    }
+}
